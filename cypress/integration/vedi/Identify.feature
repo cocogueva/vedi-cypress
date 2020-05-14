e05752fea0b5e46897e56b24cf39aa684fd5c4bb
Jessica N <jesnimu@gmail.com>
2020-05-14 17:57:17 -0500

    Identify feature basiclly donde (outline to do)

--- cypress/integration/vedi/Identify.feature
+++ cypress/integration/vedi/Identify.feature
@@ -4,9 +4,9 @@ Feature: Users can indentify themselve
    I want to identify myself as to the app with my document number 
 
         @tag-to-include
-        Scenario: opening VEDI in google chrome
+        Scenario: Bank's customer identifies himself
             Given I acces to the VEDI web
-            When I select product "1"
-            And I identify myself
-            Then  I can authenticate
+            When I select product "Cuenta Digital"
+            And I identify myself with my "45451696"
+            Then  VEDI identifies me as "JORGE" and 0
             
--- cypress/integration/vedi/Identify/identify_steps.js
+++ cypress/integration/vedi/Identify/identify_steps.js
@@ -3,44 +3,57 @@ import { Given, When, Then, And, when } from "cypress-cucumber-preprocessor/step
 const url = "https://www.abretucuenta.viabcp.com/#/home"//"https://apeu2appvedic01.azurewebsites.net/#/home";
 
 Given(`I acces to the VEDI web`, () => {
-  cy.visit(url);
+  cy.server()
+  .route('GET', '/assets/i18n/es.json').as('es.json')
+
+  .visit(url)
+  .url().should('include', '/#/home')
   
-  //Wait until de apis load
-  //Title assertion
+  
+  .get('@es.json').its('status').should('deep.equal', 200)
+  // Example: assert response property before proceeding
+  //cy.wait('@alias').its('status').should('eq', 200)
 });
 
-when
+When(`I select product {string}`, (tipoCuenta) => {
+  cy.contains(tipoCuenta).click() 
+  //cy.get('.btn.btn-primary').eq(4).click()
 
-When(`I select product "1"`, () => {
-  var accntBtns = cy.get('.btn.btn-primary')
-  accntBtns.eq(4).click()
+  if (tipoCuenta == 'Cuenta Premio') { tipoCuenta = 'CERBCP'
+  } else if (tipoCuenta == 'Cuenta Ilimitada') {tipoCuenta = 'LIBBCP'
+  } else if (tipoCuenta == 'Cuenta Digital') { tipoCuenta = 'PRICTA'
+  } else if (tipoCuenta == AFP) {tipoCuenta = '020'}
   
-
+  cy.url().should('include', '#/identificar-usuario?codProd='+tipoCuenta)
   //New URL assertion
   });
 
-And(`I identify myself`, () => {
+And(`I identify myself with my {string}`, (numDoc) => {
 
   cy.server()
-  cy.route('POST', '/channel/vedi/account-opening/v2/user-identification/user-information').as('user-information')
+  .route('POST', '/channel/vedi/account-opening/v2/user-identification/user-information').as('user-information')
 
-  cy.get('#txtDNI').type('45451696')
+  .get('#txtDNI').type(numDoc)
 
-  // Example: assert response property before proceeding
-  //cy.wait('@alias').its('status').should('eq', 200)
-
-  cy.wait('@user-information',{"timeout":15000}).should((xhr) => {
+  //Delay to wait for the user CAPTCHA validation (Manually)
+  .wait('@user-information',{"timeout":15000}).should((xhr) => {
 
     expect(xhr.status, 'successful POST').to.equal(200)
+    //expect(xhr.response,)
     //expect(xhr.url, 'post url').to.match(/\/posts$/)
     // assert any other XHR properties
   })
-
 }); 
 
+Then(`VEDI identifies me as {string} and {int}`, (name,path) => {
 
-Then(`I can authenticate`, () => {
-//cy.wait(100)
+  cy.get('@user-information').its('response').then((res) => {
+    
+    expect(res.body).to.include({
+      "names": name ,"path": path
+    })
+
+    cy.url().should('include', '/#/requisitos')
+  })
+
 });
-//Then  I can atuhenticate
-            
